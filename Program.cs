10a1db1407adcb73880360b57bfdab103e531e7c
Changhua.Li <changhua.li@hotmail.com>
2025-08-28 11:14:29 +0200

    Backend: change CORS settings for frontend integration authentication requests

--- Program.cs
+++ Program.cs
@@ -36,7 +36,8 @@ builder.Services.AddCors(options =>
         {
             builder.WithOrigins("http://localhost:4200")
                    .AllowAnyHeader()
-                   .AllowAnyMethod();
+                   .AllowAnyMethod()
+                   .AllowCredentials();
         });
 });
 
